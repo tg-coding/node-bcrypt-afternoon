b8ddb848b5ac16aa80d10376bd9a1c605a279e7f
Tia Gollaher <tiaag2013@gmail.com>
2020-01-30 16:22:15 -0700

    finished all steps

--- server/index.js
+++ server/index.js
@@ -2,11 +2,34 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const massive = require('massive');
+const authCtrl = require('./controllers/authController');
+const treasureCtrl = require('./controllers/treasureController');
+const auth = require('./middleware/authMiddleware');
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(
+    session({
+    resave: true,
+    saveUninitialized: false,
+    secret: SESSION_SECRET
+}))
+
+
+// auth endpoints
+app.post('/auth/register', authCtrl.register);
+app.post('/auth/login', authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
+
+// api endpoints
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
+
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db);
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Header.css';
 
 export default class Header extends Component {
@@ -28,15 +29,28 @@ export default class Header extends Component {
   }
 
   login() {
-    // axios POST to /auth/login here
+    const {username, password} = this.state;
+    axios.post('/auth/login', {username, password}).then (res => {
+      this.props.updateUser(res.data);
+      this.setState({username: '', password: ''});
+    }).catch(err => alert(err.response.request.response));
   }
 
   register() {
-    // axios POST to /auth/register here
+    const {username, password, isAdmin} = this.state;
+    axios.post('/auth/register', {username, password, isAdmin}).then(res => {
+      this.setState({username: '', password: ''});
+      this.props.updateUser(res.data);
+    }).catch(err => {
+      this.setState({ username: '', password: '' })
+      alert(err.response.request.response)
+  })
   }
 
   logout() {
-    // axios GET to /auth/logout here
+    axios.get('/auth/logout').then(() => {
+      this.props.updateUser({});
+    }).catch(err => console.log(err));
   }
 
   render() {
